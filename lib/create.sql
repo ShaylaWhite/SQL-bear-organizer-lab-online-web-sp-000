ac4234874f9a55ecad51f30b62fa86996ecc5ede
Shayla White <shayladwhite1@gmail.com>
2020-01-13 04:07:35 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -7,3 +7,12 @@ CREATE TABLE bears (
   temperament TEXT,
   alive INTEGER
 );
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY, 
+  title TEXT, 
+  category TEXT, 
+  funding_goal INTEGER, 
+  start_date INTEGER, 
+  end_date INTEGER
+);
+
